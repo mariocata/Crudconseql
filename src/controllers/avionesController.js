44eaf6f31a131ed0bbe1116e2ac3689215f95027
mariocata <44073884+mariocata@users.noreply.github.com>
2019-03-04 14:14:17 -0300

    Add files via upload

--- src/controllers/avionesController.js
+++ src/controllers/avionesController.js
@@ -71,6 +71,23 @@ controller.edit  = (req, res) => {
 };
 controller.update = (req, res) => {
     const { idavion }= req.params;
+    if(req.body.idproveedor == ""){
+        req.body.idproveedor = null;
+    }
+
+    if(req.body.precio_dist_recorrida == ""){
+        req.body.precio_dist_recorrida = null;
+    }
+
+    if(req.body.id_mantenimiento == ""){
+        req.body.id_mantenimiento= null;
+    }
+
+    if(    req.body.estado == ""){
+        req.body.estado= null;
+    }
+
+
     avion.update(
         { fabricante:req.body.fabricante,modelo:req.body.modelo,vel_max:req.body.vel_max,vel_crucero:req.body.vel_crucero,cant_asi_eco:req.body.cant_asi_eco,cant_asi_pri:req.body.cant_asi_pri,peso_vacio:req.body.peso_vacio,peso_max:req.body.peso_max,carga_max_equip:req.body.carga_max_equip,carga_max_cabina:req.body.carga_max_cabina,pilotos_nece:req.body.pilotos_nece,
             tipo_combus:req.body.tipo_combus,cant_combus:req.body.cant_combus,cant_banos:req.body.cant_banos,cant_salidas_emer:req.body.cant_salidas_emer,equip_medi:req.body.equip_medi,dist_depegue_crgmax:req.body.dist_depegue_crgmax,estado:req.body.estado,id_mantenimiento:req.body.id_mantenimiento,precio_dist_recorrida:req.body.precio_dist_recorrida, idproveedor:req.body.idproveedor },
